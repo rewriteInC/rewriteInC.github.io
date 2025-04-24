6d61803d31c34a84938745894ff9cf925255acb9
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-04-24 01:30:16 +0000

    [src/index.html] Add more `meta` tags for SEO
    Triggered by commit: https://github.com/rewriteInC/rewriteinc-www/commit/bf3bf5d30abeac6ff6310c299b515a4bc6734f60

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 23639, hash: 'b8ec92618f669a5b624105e0a5c39957b22719bccb14c9ccbdc357d55731cba9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17213, hash: '8d91fb7ca7f1667449e36f76e8af62696ff19752d7886a1289d4580f18b57e43', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 29917, hash: '8e7cd3067c6002ce556268a5f80acdc5db46fe64ecce8bea94b27da97162f012', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 23851, hash: '5e772de0847bce3add481daed4a2e7641e316a9f2b6fab5d556d87f110c9bcff', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17427, hash: '5dc8d952fbb0a863e73e760c7d9c4dc6d2638ff640b8139a83e7b4a6e24f2a85', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 30129, hash: 'aa8275b37fbab1d83128b2214772ae779571a1cd736229d25c96e00c85895385', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-36AW6TKX.css': {size: 6979, hash: 'vY6tjD/ce7M', text: () => import('./assets-chunks/styles-36AW6TKX_css.mjs').then(m => m.default)}
   },
 };
