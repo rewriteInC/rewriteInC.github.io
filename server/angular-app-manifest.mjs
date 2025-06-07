0adb076f42822eb568e164555e4bdfcf2bc146c6
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-06-07 04:20:24 +0000

    Angular 20
    Triggered by commit: https://github.com/rewriteInC/rewriteinc-www/commit/47aa91fb08d507f301642dddef90fc54d9f54b47

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 23851, hash: '5e772de0847bce3add481daed4a2e7641e316a9f2b6fab5d556d87f110c9bcff', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17427, hash: '5dc8d952fbb0a863e73e760c7d9c4dc6d2638ff640b8139a83e7b4a6e24f2a85', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 30129, hash: 'aa8275b37fbab1d83128b2214772ae779571a1cd736229d25c96e00c85895385', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 23851, hash: '95a56f2d47d3089c46d5654bf0832fbfa496a6799cce5d8593f7d782f3cbdd2a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17427, hash: '1461c9a85c03fc1419ac7c7bf71bae36411ab0102fea9026e2ce5feb355b0b97', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 30129, hash: '983ca427847b9c3f48dbe6fbd78190732e3137e4b03218d6ce9cab6a6b187654', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-36AW6TKX.css': {size: 6979, hash: 'vY6tjD/ce7M', text: () => import('./assets-chunks/styles-36AW6TKX_css.mjs').then(m => m.default)}
   },
 };
