546ecc4dc5ecc08c2140344540d7caa3d88be45a
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-01-21 04:16:01 +0000

    `ng update @angular/cli @angular/core @angular/material`
    Triggered by commit: https://github.com/rewriteInC/rewriteinc-www/commit/79a8ec8f4a43093092ddd711c5732b111134079d
    GitHub Actions build: https://github.com/rewriteInC/rewriteinc-www/actions/runs/21197000191

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 23851, hash: '95a56f2d47d3089c46d5654bf0832fbfa496a6799cce5d8593f7d782f3cbdd2a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17427, hash: '1461c9a85c03fc1419ac7c7bf71bae36411ab0102fea9026e2ce5feb355b0b97', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 30129, hash: '983ca427847b9c3f48dbe6fbd78190732e3137e4b03218d6ce9cab6a6b187654', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-36AW6TKX.css': {size: 6979, hash: 'vY6tjD/ce7M', text: () => import('./assets-chunks/styles-36AW6TKX_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 23806, hash: '1906bd4ffa5884b30d09b8ec491d8716f3e304e9d05bfe76f148b3c701872cb9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17427, hash: 'a471aa1941733b4f342ad6d7ca19d43a387246ba8b51c788024109c7446ddfef', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 30083, hash: 'b2e59cd58c2f8fb53096cdb2df7f8d141aaae8002ea70a8f490b2b777aa5ee32', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-Z5UYKNDX.css': {size: 6934, hash: 'VxIyzD7LHG8', text: () => import('./assets-chunks/styles-Z5UYKNDX_css.mjs').then(m => m.default)}
   },
 };
